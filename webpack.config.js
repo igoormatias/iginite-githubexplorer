6f753ee0cee74b4a3440672ffd4bd39a72da34ab
Igor Matias <igorhmatias@gmail.com>
2021-03-08 22:42:49 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+module.exports = {
+  mode: isDevelopment ?'development' : 'production',
+  devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public')
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js|\.jsx$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+       
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+};
